1975f6f47b19162d627d51478430abe054db7f7d
EC2 Default User <ec2-user@ip-172-31-5-205.ap-northeast-1.compute.internal>
2022-12-21 12:12:16 +0000

    logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,9 @@ class ApplicationController < ActionController::Base
              user_path(current_user.id)
     end
     
+    def after_sign_out_path_for(resource)
+             root_path
+    end
     
     
     
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,3 +1,5 @@
 class HomesController < ApplicationController
-    
+    def top
+        
+    end
 end
