ebc6ce01f85555e3f4838a809c73cd610dce4bb3
EC2 Default User <ec2-user@ip-172-31-5-205.ap-northeast-1.compute.internal>
2022-12-25 08:56:24 +0000

    388

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
     private
     
     def user_params
-        params.require(:user).permit(:name,:image,:introduction)
+        params.require(:user).permit(:name,:profile_image,:introduction)
     end
     
     def correct_user
